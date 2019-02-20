a48e027cfb278ea1efddcb6a70972a67cfd2fb03
scienclick <scienclick@gmail.com>
2019-02-20 03:17:21 -0800

    a

--- my_prj/settings.py
+++ my_prj/settings.py
@@ -77,7 +77,6 @@ WSGI_APPLICATION = 'my_prj.wsgi.application'
 
 DATABASES = {
     'default': {
-
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'db0',
         'USER':'dbuser',
