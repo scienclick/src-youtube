ced79c01ab7f1125b8f288f84d6669f6684f89e3
scienclick <scienclick@gmail.com>
2019-02-19 20:13:39 -0800

    a

--- my_prj/settings.py
+++ my_prj/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'my_prj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'db0',
+        'USER':'dbuser',
+        'PASSWORD':'root12345',
+        'HOST':'localhost',
+        'PORT':"",
     }
+
+
 }
 
 
